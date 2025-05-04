8b5c9d1be4dc8f135dce3e85243a852fa6a9e7ad
Mikkel Rask <mikkelrask@pm.me>
2025-05-04 12:45:24 +0200

    Trying stuff blindly
    - up tauri-build version to match tauri version
    - add tauri::Manager to `main.rs`

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use std::{env, path::PathBuf, process::Command, thread, time::Duration, net::TcpStream, io::ErrorKind};
+use tauri::Manager; 
 
 // Function to check if server is ready
 fn wait_for_server(host: &str, port: u16, timeout_secs: u64, retry_interval_ms: u64) -> bool {
